cfa7f6ff2ccf6638d7e359b246259e91969ba280
Ramineni Nandu Varma <123319320+Nandu-ramineni@users.noreply.github.com>
2024-05-23 23:07:09 +0530

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyA6twFC0B6TVsipvw9-ISCfQAuHOATee10",
-  authDomain: "job-portal-c3290.firebaseapp.com",
-  projectId: "job-portal-c3290",
-  storageBucket: "job-portal-c3290.appspot.com",
-  messagingSenderId: "259404230219",
-  appId: "1:259404230219:web:71fb9cdb7c5593b3dbe7ce",
-  measurementId: "G-W7CB1F5HEL"
+  //
 };
 
 // Initialize Firebase
